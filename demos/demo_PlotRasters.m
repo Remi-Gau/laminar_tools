38c91058afdc9132e1b0152b30ef792c7e6a22a8
Remi Gau <remi_gau@hotmail.com>
2021-03-27 19:41:55 +0100

    refactor demo

--- demos/demo_PlotRasters.m
+++ demos/demo_PlotRasters.m
@@ -4,58 +4,87 @@ clear;
 close all;
 clc;
 
-OneRoi;
+Opt.NbSubject = 1;
+Opt.NbRuns = 20;
+Opt.NbLayers = 6;
+Opt.NbVertices = 1000;
 
-function OneRoi
+% use iid data across layer or add some layer error covariance
+Opt.IID = true;
 
-    Opt.NbSubject = 1;
-    Opt.NbRuns = 20;
-    Opt.NbLayers = 6;
-    Opt.NbVertices = 1000;
+% FWHM for smotthing "across vertices"
+% proportion of tne number of vertices
+Opt.Raster.VerticalFWHM = 1 / 500;
+Opt.Raster.ColorMap = SeismicColorMap(1000);
+Opt.Raster.AddXLabel = true;
 
-    % use iid data across layer or add some layer error covariance
-    Opt.IID = true;
+Opt.Raster.Sort = true;
+Opt.Raster.CrossValidate = true;
 
+%%
+OneRoi(Opt)
+
+%%
+OneRoTwoConditions(Opt)
+
+function OneRoi(Opt)
+    
     ROI = 1;
     Cdt = 1;
-
     Opt = SetParametersProfileSimulation(Opt, ROI, Cdt);
-
-    Opt.StdDevWithinSubject = Opt.Betas(1) * 20;
-
+    Opt.StdDevWithinSubject = Opt.Betas(1) * 15;
     Data = GenerateSubjectSurfaceDataLaminarProfiles(Opt);
-
+    
     SortingData = Data;
-
-    Opt.Raster.Sort = true;
-    Opt.Raster.CrossValidate = true;
-
+    
     [Data, SortingData, R] = SortRaster(Data, SortingData, Opt, 'Cst');
-
-    % proportion of FWHM for
-    Opt.Raster.VerticalFWHM = 1 / 100;
-    Opt.Raster.ColorMap = SeismicColorMap(1000);
-
-    Opt.Raster.AddXLabel = true;
-
+    
     [~, ~, Opt] = GetPlottingDefaults(Opt);
-
+    
+    figure
+    
     subplot(121);
     Opt.Title = 'ROI 1 - Sorting condition';
     PlotOneRaster(mean(SortingData, 3), Opt);
-
+    
     subplot(122);
     Opt.Title = 'ROI 1 - Sorted condition';
     Opt.Raster.AddColorBar = true;
     PlotOneRaster(mean(Data, 3), Opt);
+    
+end
 
-    % Opt.Specific{1}.Data = Data;
-    % Opt.Specific{1}.SubjectVec = SubjectVec;
-    % Opt.Specific{1}.ConditionVec = ones(size(Data, 1), 1);
-    % Opt.Specific{1}.RoiVec = ones(size(Data, 1), 1);
-    %
-    % Opt.Specific{1}.XLabel = {'ROI 1'};
 
-    % PrintFigure(pwd);
-
+function OneRoTwoConditions(Opt)
+    
+    % TODO
+    % Generating correlated conditions will require tweaking the
+    % variance-covariance matrix used to generate data with
+    % MVNRND
+    
+    ROI = 1;
+    Cdt = 1;
+    Opt = SetParametersProfileSimulation(Opt, ROI, Cdt);
+    Data = GenerateSubjectSurfaceDataLaminarProfiles(Opt);
+    
+    ROI = 1;
+    Cdt = 2;
+    Opt = SetParametersProfileSimulation(Opt, ROI, Cdt);
+    SortingData = GenerateSubjectSurfaceDataLaminarProfiles(Opt);
+    
+    [Data, SortingData, R] = SortRaster(Data, SortingData, Opt, 'Cst');
+    
+    [~, ~, Opt] = GetPlottingDefaults(Opt);
+    
+    figure
+    
+    subplot(121);
+    Opt.Title = 'ROI 1 - Sorting condition';
+    PlotOneRaster(mean(SortingData, 3), Opt);
+    
+    subplot(122);
+    Opt.Title = 'ROI 1 - Sorted condition';
+    Opt.Raster.AddColorBar = true;
+    PlotOneRaster(mean(Data, 3), Opt);
+    
 end
